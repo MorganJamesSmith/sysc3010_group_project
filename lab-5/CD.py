77bff1ec2b801e6ef7c055533b5bf9785187527d
Morgan Smith <Morgan.J.Smith@outlook.com>
2020-10-15 10:39:56 -0400

    feat: Complete section D for lab 5

--- lab-5/CD.py
+++ lab-5/CD.py
@@ -1,7 +1,10 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
 
+from D import *   # Line added by Morgan
+
 def function_CD():
+    function_D()   # Line added by Morgan
     print ("CD")
     
 
