c86fcd08ce26706522884e0eb70ff91038e42697
sunnypujari <sunjeevanipujari@cmail.carleton.ca>
2020-10-15 12:27:27 -0400

    new B.py file created and AB.py modified

--- lab-5/AB.py
+++ lab-5/AB.py
@@ -1,9 +1,11 @@
 # SYSC3010 Lab 5 - AB.py
-#     The file to be jointly edited by Team Members A and B
+# The file to be jointly edited by Team Members A and B
 
 from A import *
+from B import *
 
 def function_AB():
     function_A()
+    function_B()
     print ("AB")
  
