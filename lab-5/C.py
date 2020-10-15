ce0c73d3476e69d26a6f378058262e6fa10c11ed
Mario Shebib <mario.shebib@carleton.ca>
2020-10-15 10:39:21 -0400

    added C.py and modified CD.py

---
+++ lab-5/C.py
@@ -0,0 +1,5 @@
+#SYSC 3010 Lab 5 C.py
+#prints out the letter C
+
+def function_C():
+    print ("C")
--- lab-5/CD.py
+++ lab-5/CD.py
@@ -1,7 +1,9 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
+from C import * #line is added my Mario Shebib Team Member C
 
 def function_CD():
+    function_C() #line is added my Mario Shebib Team Member C
     print ("CD")
     
 
