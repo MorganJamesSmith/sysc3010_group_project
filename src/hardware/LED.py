1381131ada3411d297d8c69d18efee0030b36ab4
Mario Shebib <38814197+Mario-Shebib@users.noreply.github.com>
2020-11-17 09:59:57 -0500

    [Mario Shebib]feat: added __init__ to LED

--- src/hardware/LED.py
+++ src/hardware/LED.py
@@ -1,3 +1,5 @@
+#GPIO pins will control the LEDs
+import RPi.GPIO as GPIO
 from enum import Enum
 #Enumeration with the 3 LED colours we have from the combination of Red and Green
 class LEDColour(Enum) :
@@ -6,35 +8,34 @@ class LEDColour(Enum) :
     YELLOW = 3
     OFF = 4
 #Class that controls the LED
-class LED 
+class LED:
     
-    #GPIO pins will control the LEDs
-    import RPi.GPIO as GPIO
-
-    red_pin_num = 17
-    green_pin =_ 18
+    #Initalizes and sets the pins
+    def __init__(self, red_pin = 17, green_pin = 18):
+        self.red_pin = red_pin
+        self.green_pin = green_pin
     
-    GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
     
-    #GPIO setup will have pin 17 and 18 as outputs controlling red and green
-    GPIO.setup(red_pin, GPIO.OUT)
-    GPIO.setup(green_pin, GPIO.OUT)
+        #GPIO setup will have pin 17 and 18 as outputs controlling red and green
+        GPIO.setup(self.red_pin, GPIO.OUT)
+        GPIO.setup(self.green_pin, GPIO.OUT)
 
     #defined method for setting LED colours
-    def set_colour(LEDColour colour)
+    def set_colour(colour):        
         #if the selected colour is red make sure that red pin is on and green pin is off
-        if (colour == RED)
-            GPIO.output(red_pin, True)
-            GPIO.output(green_pin, False)        
+        if (colour == RED):
+            GPIO.output(self.red_pin, True)
+            GPIO.output(self.green_pin, False)        
         #if the selected colour is red make sure that red pin is on and green pin is off
-        elif (colour == GREEN)
-            GPIO.output(red_pin, False)
-            GPIO.output(green_pin, True)
+        elif (colour == GREEN):
+            GPIO.output(self.red_pin, False)
+            GPIO.output(self.green_pin, True)
         #if the selected colour is red make sure that red pin is on and green pin is on
-        elif (colour == YELLOW)
-            GPIO.output(red_pin, True)
-            GPIO.output(green_pin, True)
-        else
-            GPIO.output(red_pin, False)
-            GPIO.output(green_pin, False)
+        elif (colour == YELLOW):
+            GPIO.output(self.red_pin, True)
+            GPIO.output(self.green_pin, True)
+        else:
+            GPIO.output(self.red_pin, False)
+            GPIO.output(self.green_pin, False)
         
