d58e7fe5305d7bae36a6138d647228c046c3a446
Mario Shebib <38814197+Mario-Shebib@users.noreply.github.com>
2020-11-18 18:45:17 -0500

    [Mario Shebib]feat: unittest for DoorNodeController

---
+++ src/test/testing-demo/DoorNodeControllerTest.py
@@ -0,0 +1,38 @@
+import sys
+
+sys.path.append("../../communication")
+
+import thingspeak
+import transport
+
+import unittest
+import DoorNodeController
+
+
+class DoorNodeControllerTest(unittest.TestCase):
+    def __init__(self,door_type, transaction_id,limit_distance):
+        self.door_node = DoorNodeController(ENTRANCE, 1, 0)
+        
+    def test_handle_access_response(self):
+        message_true = AccessResponseMessage(1,True)
+        self.assertTrue(self.door_node.handle_access_response(self, message_true))
+        message_false = AccessResponseMessage(2, False)
+        self.assertFalse(self.door_node.handle_access_response(self, message_false))
+    
+    def test_handle_badge_tap(self, badge_id):
+        message = AccessRequest(1,badge_id)
+
+    
+    def test_handle_information_request(self, message):
+
+        ambient_temp = self.ir_temp_sensor.get_ambient_temp()
+        person_temp = self.ir_temp_sensor.get_ir_temp()
+        payload = TemperatureInfoPayload(ambient_temp,person_temp)
+        message = InformationResponseMessage(self.tid, InformationType.USER_TEMPERATURE, payload)
+    
+    def test_handle_door_state_update(self, message):
+        message_allow = DoorStateUpdateMessage(DoorState.ALLOWING_ENTRY)
+        self.assertTrue(self.door_node.handle_door_state_update(self, message_allow))
+        message_not_allow = AccessResponseMessage(DoorState.NOT_ALLOWING_ENTRY)
+        self.assertFalse(self.door_node.handle_door_state_update(self, message_not_allow))       
+
