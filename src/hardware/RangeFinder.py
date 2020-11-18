1652558e7b2633f1f85ce79e07956691faa570f4
sunnypujari <71533088+sunnypujari@users.noreply.github.com>
2020-11-18 18:33:12 -0500

    [Sunny] feat: added a variable to the get range function

--- src/hardware/RangeFinder.py
+++ src/hardware/RangeFinder.py
@@ -1,3 +1,10 @@
+#! /usr/bin/env python3
+
+#
+# file name: tof_code_v1
+# Description: RangeFinder functional code
+# Author(s): Sunjeevani Pujari 
+#
 import time
  
 import board
@@ -7,13 +14,13 @@ import adafruit_vl53l0x
 
 class RangeFinder:
     
-    def __init__(self,activate):
-        self.activate = activate
+    def __init__(self):
         #self.address= address
         # Initialize I2C bus and sensor.
         i2c = busio.I2C(board.SCL, board.SDA)
         vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-    def get_range(self):
+    def get_range(self,activate):
+        self.activate = activate
         if self.activate == True:
             dist = vl53.range
             return dist
