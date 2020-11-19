549dadb2d6ba632aa0d4fa515ca8cbd63d68c856
sunnypujari <71533088+sunnypujari@users.noreply.github.com>
2020-11-18 21:52:17 -0500

    [Sunny] feat: add tuples to binding statements

--- src/database_code.py
+++ src/database_code.py
@@ -23,7 +23,7 @@ class DataBase:
         try:
             self.badge_id = badge_id
             #Variables needed from database
-            self.database.execute("SELECT employee_id FROM nfc_and_employee_id WHERE nfc_id =?", self.badge_id)
+            self.database.execute("SELECT employee_id FROM nfc_and_employee_id WHERE nfc_id =?", (self.badge_id,))
             employeeId = self.database.fetchone()[0]
             
             if employeeId == NULL:
@@ -33,11 +33,11 @@ class DataBase:
                 #if AccessRequestMessage is from exit node, all we need is to get employee ID
                 return employeeId
             #if AccessRequestMessage is from entry node, we need information about employees most recent entry attempt
-            self.database.execute("SELECT access_date FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", employeeId)
+            self.database.execute("SELECT access_date FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", (employeeId,))
             accessDate = self.database.fetchone()[0]
-            selfdatabase.execute("SELECT status FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", employeeId)
+            selfdatabase.execute("SELECT status FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", (employeeId,))
             statusType = self.database.fetchone()[0]
-            self.database.execute("SELECT validity FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", employeeId)
+            self.database.execute("SELECT validity FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", (employeeId,))
             validity = self.database.fetchone()[0]
             return employeeID, accessDate,statusType, validity
             
@@ -49,9 +49,9 @@ class DataBase:
         self.employeeID = employeeId #INT value of employee ID acquired from the nfc ID in the AccessRequestMessage 
         self.exitnode = exitnode #TEXT value of the entry node requesting entry
         #fields saved to log exit access of employee
-        self.database.execute("INSERT INTO access_exit (employee_id, exit_time,exit_date,exit_node) VALUES (?,?,?,?)",self.employeeId,time('now'),date('now'),self.exitnode)
+        self.database.execute("INSERT INTO access_exit (employee_id, exit_time,exit_date,exit_node) VALUES (?,?,?,?)",(self.employeeId,time('now'),date('now'),self.exitnode))
         self.database.commit()
-        self.database.execute("SELECT * FROM access_exit WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", employeeId)
+        self.database.execute("SELECT * FROM access_exit WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", (employeeId,))
         print ("data added to the database is:",database.fetchone())
         
         if (self.database):
@@ -66,9 +66,9 @@ class DataBase:
         self.status = status #TEXT value of the status of employee 
         #fields saved to log exit access of employee
         self.database.execute("INSERT INTO access_entry (employee_id,access_time,access_date,access_node,in_range,temp_reading,status,validity) VALUES(?,?,?,?,?,?,?,?)",
-                         self.employeeId,time('now'),date('now'),self.entrynode,'Y',self.tempReading,self.status,0)
+                         (self.employeeId,time('now'),date('now'),self.entrynode,'Y',self.tempReading,self.status,0))
         self.database.commit()
-        self.database.execute("SELECT * FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", employeeId)
+        self.database.execute("SELECT * FROM access_entry WHERE employee_id = ? ORDER BY employee_id DESC LIMIT 1", (employeeId,))
         print ("data added to the database is:",database.fetchone())
         if (self.database):
             self.database.close()
