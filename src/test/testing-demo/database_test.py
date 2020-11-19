240d09aacff0ba5cc124b1f939fb76787d693487
sunnypujari <71533088+sunnypujari@users.noreply.github.com>
2020-11-18 19:52:05 -0500

    [Sunny] Bug: fixing class variables

--- src/test/testing-demo/database_test.py
+++ src/test/testing-demo/database_test.py
@@ -6,22 +6,22 @@
 # Author(s): Sunjeevani Pujari
 #
 
-from database_code import *
+from database_test import DataBase
 
 badge_id = bytes.fromhex("68091a96e619ad7997099f16c64b5e17")
 
 #exit loop
 exitnode = "North exit"
-employee_id = database_test.accessrequest('exit',badge_id)
+employee_id = DataBase.accessrequest("exit",badge_id)
 print(f"employee ID of employee requesting exit is:",employee_id)
 #logging information for exit process
-database_test.exit_log(employee_id,exitnode)
+DataBase.exit_log(employee_id,exitnode)
 
 #entry loop
 entrynode = "Sounth entry"
-employee_id ,access_date,status,validity= database_test.accessrequest('entry',badge_id)
+employee_id ,access_date,status,validity= DataBase.accessrequest("entry",badge_id)
 print(f"employee ID of employee requesting entry is:",employee_id)
 #logging information for entry process
 temp_reading = 37
 status = "authorized"
-database_test.entry_log(employee_id,entrynode,temp_reading,status)
+DataBase.entry_log(employee_id,entrynode,temp_reading,status)
