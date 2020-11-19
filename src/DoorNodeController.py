fdcfffbc101f40e77764ba3b37002f6c4f55ac87
Mario Shebib <38814197+Mario-Shebib@users.noreply.github.com>
2020-11-18 22:34:00 -0500

    [Mario Shebib]fix: DoorNodeController fixes
    modified methods to help a working demo be created for testing demo

--- src/DoorNodeController.py
+++ src/DoorNodeController.py
@@ -24,15 +24,20 @@ class DoorNodeController:
         self.current_state = DoorState
         self.door_type = DoorType
 
-        self.indicator = LED(17,18)
+        self.indicator = LED.LED(17,18)
         self.nfc_reader = RC522
         self.range_finder = RangeFinder
-        self.ir_temp_sensor = MLX90614
-        self.door_lock = ElectronicDoorLock
+        self.ir_temp_sensor = mlx90614.MLX90614
+        self.door_lock = DoorActuator.DoorActuator
 
         self.tid = transaction_id
 
         self.dist_from_temp_sensor = limit_distance
+
+        self.indicator.set_colour(LED.LEDColour.RED)
+
+    
+    def main_loop(self):
         # Get API keys from file
         try:
             with open("api_write_key.txt", "r") as keyfile:
@@ -47,12 +52,7 @@ class DoorNodeController:
         channel = thingspeak.Channel(1222699, write_key=write_key, read_key=read_key)
 
         self.connection = transport.Connection(channel, door_type(str), "control_server")
-        self.connection.established.wait()
-        self.indicator.set_colour(RED)
-
-    
-    def main_loop(self):
-  
+        self.connection.established.wait()  
         while True:
             
             badge_id = nfc_reader.read_card()
@@ -70,10 +70,10 @@ class DoorNodeController:
                 distance = 0
                 distance = range_finder.get_range()
                 if(distance > self.dist_from_temp_sensor) :
-                    self.indicator.set_colour(YELLOW)
+                    self.indicator.set_colour(LEDColour.YELLOW)
                 while(distance > self.dist_from_temp_sensor) :
                     distance = range_finder.get_range()
-                self.indicator.set_colour(RED)                
+                self.indicator.set_colour(LEDColour.RED)                
                 # Send information response
                 handle_information_request(rsp)
             elif type(rsp) == AccessResponseMessage:
@@ -108,12 +108,12 @@ class DoorNodeController:
     def handle_door_state_update(self, message):
           
         if(message.state == DoorState.ALLOWING_ENTRY):
-            self.indicator.set_colour(RED)
-            self.door_state = rsp.state
+            self.indicator.set_colour(LED.LEDColour.RED)
+            self.door_state = message.state
             return self.door_state
-        elif(message.state == DoorState.NOT_ALLLOWING_ENTRY):
-            self.indicator.set_colour(OFF)
-            self.door_state = rsp.state
+        elif(message.state == DoorState.NOT_ALLOWING_ENTRY):
+            self.indicator.set_colour(LED.LEDColour.OFF)
+            self.door_state = message.state
             return self.door_state
         else:
             print("Fail did not receive a valid DoorState update message")
@@ -122,12 +122,12 @@ class DoorNodeController:
     def handle_access_response(self, message):
 
         if message.accepted == True:
-            self.indicator.set_colour(GREEN)
-            self.door_lock.door_open()
+            self.indicator.set_colour(LED.LEDColour.GREEN)
+            #self.door_lock.open(self)
             return True
         else:
             #door stays locked
-            self.door_lock.door_lock()
+            #self.door_lock.lock(self)
             return False
     
 #Enumeration with the for if a DoorNodeController is an entrance or exit
