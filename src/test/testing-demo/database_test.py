0042642e5b352b6ffa8fd26c857088450ab47a06
sunnypujari <71533088+sunnypujari@users.noreply.github.com>
2020-11-18 20:50:06 -0500

    [Sunny] feat: added an instance of Database class and added additional code to add entries to database

--- src/test/testing-demo/database_test.py
+++ src/test/testing-demo/database_test.py
@@ -8,20 +8,28 @@
 
 from database_code import DataBase
 
-badge_id = bytes.fromhex("68091a96e619ad7997099f16c64b5e17")
+db = DataBase()
+
+badge_id1 = bytes.fromhex("68091a96e619ad7997099f16c64b5e17")
+
+badge_id2 = bytes.fromhex("63d584ccd569b75faf69d1a8e230efc7")
+
+#adding info to database
+db.add_entries(badge_id1,1)
+db.add_entries(badge_id2,2)
 
 #exit loop
 exitnode = "North exit"
-employee_id = DataBase.accessrequest("exit",badge_id)
+employee_id = db.accessrequest("exit",badge_id)
 print(f"employee ID of employee requesting exit is:",employee_id)
 #logging information for exit process
-DataBase.exit_log(employee_id,exitnode)
+db.exit_log(employee_id,exitnode)
 
 #entry loop
 entrynode = "Sounth entry"
-employee_id ,access_date,status,validity= DataBase.accessrequest("entry",badge_id)
+employee_id ,access_date,status,validity= db.accessrequest("entry",badge_id)
 print(f"employee ID of employee requesting entry is:",employee_id)
 #logging information for entry process
-temp_reading = 37
+temp_reading = 37.0
 status = "authorized"
-DataBase.entry_log(employee_id,entrynode,temp_reading,status)
+db.entry_log(employee_id,entrynode,temp_reading,status)
