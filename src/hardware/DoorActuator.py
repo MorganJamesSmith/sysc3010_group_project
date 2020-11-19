6f329cd6557995fefa5b70792cabe26b20bcdfd5
Morgan Smith <Morgan.J.Smith@outlook.com>
2020-11-18 19:22:02 -0500

    [Morgan Smith] chore: convert DoorActuator into a class

--- src/hardware/DoorActuator.py
+++ src/hardware/DoorActuator.py
@@ -6,55 +6,52 @@
 #
 # Functions for controlling the door actuator
 
-# TODO: Allow for one node to actuate multiple doors
-
 from time import sleep
 import RPi.GPIO as GPIO
 
-locked = 12.5
-unlocked = 2.5
-servo_delay = 0.5
+class DoorActuator:
 
-p = ""
 
-def door_clean_up():
-    p.stop()
-    GPIO.cleanup()
+    def clean_up(self):
+        self.p.stop()
+        GPIO.cleanup()
 
-def door_setup(servo_pin, frequency):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(servo_pin, GPIO.OUT)
-    global p
-    p = GPIO.PWM(servo_pin, frequency)
-    p.start(locked)
-    sleep(servo_delay)
+    def __init__(self, servo_pin, frequency):
+        self.locked = 12.5
+        self.unlocked = 2.5
+        self.servo_delay = 0.5
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(servo_pin, GPIO.OUT)
+        self.p = GPIO.PWM(servo_pin, frequency)
+        self.p.start(self.locked)
+        sleep(self.servo_delay)
 
-# Locks the door
-def door_lock():
-    p.ChangeDutyCycle(locked)
-    sleep(servo_delay)
-    print("Door locked!")
+    # Locks the door
+    def lock(self):
+        self.p.ChangeDutyCycle(self.locked)
+        sleep(self.servo_delay)
+        print("Door locked!")
 
-# Unlocks the door
-def door_open():
-    p.ChangeDutyCycle(unlocked)
-    sleep(servo_delay)
-    print("Door unlocked!")
+    # Unlocks the door
+    def open(self):
+        self.p.ChangeDutyCycle(self.unlocked)
+        sleep(self.servo_delay)
+        print("Door unlocked!")
 
-# Unlocks the door for delay seconds and then locks the door
-def door_open_timed(delay):
-    p.ChangeDutyCycle(unlocked)
-    print("Door unlocked for", delay, "seconds!")
-    sleep(delay)
-    p.ChangeDutyCycle(locked)
-    print("Door locked!")
-    sleep(servo_delay)
+    # Unlocks the door for delay seconds and then locks the door
+    def open_timed(self, delay):
+        self.p.ChangeDutyCycle(self.unlocked)
+        print("Door unlocked for", delay, "seconds!")
+        sleep(delay)
+        self.p.ChangeDutyCycle(self.locked)
+        print("Door locked!")
+        sleep(self.servo_delay)
 
 
 # Quick test of functions
 if __name__ == "__main__":
-    door_setup(12, 50)
-    door_open()
-    door_lock()
-    door_open_timed(1)
-    door_clean_up()
+    door = DoorActuator(12, 50)
+    door.open()
+    door.lock()
+    door.open_timed(1)
+    door.clean_up()
